8b3df8dcfac17720b3455a12e24f842f4776f867
psychicalcoder <psychicalcoder@gmail.com>
2023-01-25 18:31:51 +0800

    add avy, yaml-mode, ox-rst, install 2 pkgs from gnu elpa

--- init.el
+++ init.el
@@ -7,8 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(company-show-quick-access t nil nil "Customized with use-package company")
  '(package-selected-packages
-   '(consult auctex rust-mode embark org-bullets lsp-pyright lsp-mode lv markdown-mode ht orderless consult-company company doom-modeline shrink-path f s doom-themes magit magit-section git-commit with-editor transient dash vertico quelpa-use-package use-package bind-key quelpa))
- '(safe-local-variable-values '((xref-etags-mode . t))))
+   '(ox-rst avy quelpa embark yaml-mode rust-mode lsp-pyright lsp-mode lv markdown-mode ht org-bullets doom-modeline shrink-path f s doom-themes magit magit-section git-commit with-editor transient dash company orderless consult vertico spinner quelpa-use-package compat)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
